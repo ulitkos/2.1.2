ca296567bee7d7d1044e7f9098eb96f2687e5fa7
vpr <vpr@mail.ru>
2021-11-17 22:57:38 +0300

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,19 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+
+
+
+    }
+}
